e7c31a053474d05fd2fa7289a9e7220398709f3f
yerin97 <92047632+yerin97@users.noreply.github.com>
2021-10-16 23:08:47 -0400

    Update app4.py

--- app/app4.py
+++ app/app4.py
@@ -25,47 +25,47 @@ def app():
     group of candidates residing in less developed cities. Our box plot distributions of CDI by Education Level show that Masters,\
     Graduates, and PhD’s are highly centered around cities with high CDI’s, while high school and primary school grads are scattered\
     towards lower CDI’s with slightly more outliers. We hypothesize that candidates in developing cities may have access to online/open-source\
-    material that can supplement or replace formal training in DS, hence the supply in the job market. \n
+    material that can supplement or replace formal training in DS, hence the supply in the job market.')
 
-    60% of the dataset are graduate students, with 60% having Graduate Degrees, 22.8% with Undergraduate, 2.15% with PhDs\
+    st.write('60% of the dataset are graduate students, with 60% having Graduate Degrees, 22.8% with Undergraduate, 2.15% with PhDs\
     and 10.5% with high school degrees. The developers found this distribution quite jarringly different from the job market situation\
     in the U.S., where closely 50-60% of the data scientist job applicants hold Master’s degrees or higher, so we deemed it a factor highly\
     dependent on the region/continent, which is unknown. The years of experience by education level is as expected with PhD’s and Master’s\
     students having the upper bound of 20> years, followed by Undergraduate degree holders, then High School and Primary Schoolers. Since Data\
     Scientists used to be primarily PhD’s or academic scholars, it’s not surprising that those groups have more experiences than others.\
     The experience distribution by major was quite contrary to our hypothesis - that STEM degree holders will have more YoE on average - with \
-    all disciplines having pretty much equivalent distributions. \n
+    all disciplines having pretty much equivalent distributions.')
 
-    We must note that our dataset’s intrinsically imbalanced in terms of the candidates’ experience and gender, with nearly ~40% of the\
+    st.write('We must note that our dataset’s intrinsically imbalanced in terms of the candidates’ experience and gender, with nearly ~40% of the\
     candidates having 20+ years of work experience as a Data Scientist. Another limitation that the dataset had was its ambiguity in certain\
     columns, including training hours, which the developers assumed as the # of hours formally trained as a data scientist apart from general\
     work experience. This information would have been more meaningful if it were the number of hours per week spent on training oneself as a\
     better Data Professional, but since it isn’t, the more relevant work experiences as a Data Scientist, the longer the training hours, hence\
-    the apparent correlation between having relevant work experience and higher training hours. \n
+    the apparent correlation between having relevant work experience and higher training hours.')
 
-    Last New Job distribution was quite interesting, with 60% of the candidates only having worked at their current job for less than a year.\
+    st.write('Last New Job distribution was quite interesting, with 60% of the candidates only having worked at their current job for less than a year.\
     Given that DS’s are predominantly hired in tech companies or at tech functions, it’s not surprising that frequent job switches are common\
-    and not necessarily frowned upon, compared to more traditional industries. \n
+    and not necessarily frowned upon, compared to more traditional industries.')
 
-    We include two gender-related distributions before deep-diving into it in Analysis 2, as the dataset has 1.5x more male than female candidates,\
+    st.write('We include two gender-related distributions before deep-diving into it in Analysis 2, as the dataset has 1.5x more male than female candidates,\
     it was hard to recognize the data points that represent female data scientists in Distribution of Data Scientists by Experience and Last New Job.\
     In almost all cases, the number of male data scientists is much higher than the female data scientists, female data scientists points were covered.\
     This graph showed that the respondents who have worked for their previous company for 4 years tended to have longer working experience.\
-    On the other hand, the ones who have shorter working experience have changed their positions or jobs more often.\n
+    On the other hand, the ones who have shorter working experience have changed their positions or jobs more often.')
 
-    To classify the data scientists by their gender and education level, we added two drop-down menus, so the users can easily select\
+    st.write('To classify the data scientists by their gender and education level, we added two drop-down menus, so the users can easily select\
     the data that matches with a certain condition and only use it to create plots. Using these two options, we created three-bar plots\
     which show the distribution of data scientists by their enrolled university types, majors, and company types. The majority of the data\
     scientists in the given data set are not currently enrolled in university. However, most Ph.D.-level data scientists answered that they\
     are currently enrolled in university. Also, the proportion of data scientists who are currently attending university full-time was much\
-    higher in the female data scientists group than in the male group.\n
+    higher in the female data scientists group than in the male group.')
 
-    In the major graph, as was expected, the majority of data scientists studied STEM majors regardless of gender,\
+    st.write('In the major graph, as was expected, the majority of data scientists studied STEM majors regardless of gender,\
     and those who did not attend university are classified as ‘other’ in the major graph. The number of data scientists who studied Arts\
-    during their undergrad was the lowest in this distribution graph.\n
+    during their undergrad was the lowest in this distribution graph.')
 
-    Lastly, to find which type of companies hire data scientists the most, we drew a graph that shows the company type and size\
+    st.write('Lastly, to find which type of companies hire data scientists the most, we drew a graph that shows the company type and size\
     that the respondents are currently working for. According to their answers, the majority works for small private firms regardless of gender.\
     However, when we selected only Ph.D.-level data scientists, the result was different. The proportion of respondents\
-    who work for the public sector has increased. ')
+    who work for the public sector has increased.')
 
