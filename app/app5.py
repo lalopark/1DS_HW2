49c159ad7cb55ba27ce89e1a7e8027a07cb026cb
yerin97 <92047632+yerin97@users.noreply.github.com>
2021-10-16 21:53:18 -0400

    Update app5.py

--- app/app5.py
+++ app/app5.py
@@ -1,3 +1,13 @@
+#app5.py
+import numpy as np
+import pandas as pd
+import altair as alt
+import plotly.express as px
+import plotly.graph_objs as go
+import pickle as pkle
+import os.path
+import streamlit as st
+
 st.title('Developer Contact')
 st.write('Su Park')
 st.write('Ye Rin Han')
