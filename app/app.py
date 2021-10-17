c0e7ba8a44d4b2ceea412e44ab4493104c6db01e
yerin97 <92047632+yerin97@users.noreply.github.com>
2021-10-16 20:15:27 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import app1
 import app2
 import app3
 import app4
+import app5
 
 import streamlit as st
 
@@ -12,7 +13,9 @@ PAGES = {
     "About the Dataset": app1,
     "Analysis1": app2,
     "Analysis2": app3,
-    "Writeup": app4
+    "Writeup": app4,
+    'Developer Contact': app5
+    
 }
 st.sidebar.title('Page Navigation')
 selection = st.sidebar.selectbox("Go to", list(PAGES.keys()))
