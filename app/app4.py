ff82c2adcff4f88fc5c288258a46bdf62afc6e6c
lalopark <92047634+lalopark@users.noreply.github.com>
2021-10-16 23:37:08 -0400

    Update app4.py

--- app/app4.py
+++ app/app4.py
@@ -11,7 +11,6 @@ import streamlit as st
 
 def app():
     st.title('Writeup')
-    st.image('app/female-ds.jpeg')
     
     st.write('We created several different visualizations of the data set from both a macro and micro lens by\
     first illustrated the general statistical distributions of the data scientist candidate population in terms\
