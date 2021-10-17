8848079a6dbfda45634613db10576125ea663c86
yerin97 <92047632+yerin97@users.noreply.github.com>
2021-10-16 20:20:22 -0400

    Update app1.py

--- app/app1.py
+++ app/app1.py
@@ -5,11 +5,17 @@ import pandas as pd
 def app():
     st.image('app/data_scientist.png',use_column_width=True)
     st.title('Context')
-    st.write('The number of companies that hire data scientists is increasing\
-            since big data analytics help companies to make informed decisions and understand their customer desires better.\
-            This data was created to learn background of current data scientists and to build a model which predicts the probability of candidates for data scientist position.')
+    st.write('The demand for data scientists has been steadily increasing as the job title is often referenced as "the sexiest job of the 21st century.” \
+    As students of data science, we were interested in diving into the HR analytics involving data science practitioners,\
+    hence obtained a dataset from Kaggle (https://www.kaggle.com/arashnic/hr-analytics-job-change-of-data-scientists)\
+    that includes the demographic, education, and experience information of data scientist candidates for a company. ')
     st.title('About Data')
-    st.write('This dataset incldues 14 features of 19093 data scientists such as demographics, education, and experience level.')
+    st.write('This dataset includes 14 features of 19093 data scientists such as demographics, education, and experience level\
+    and is divided into test and train, the latter containing an additional column of “whether the candidate is currently looking\
+    for a job” which we’ve utilized to create an interactive sidebar feature to collect the user’s input responses to predict their\
+    likelihood of looking for a new job at the moment, based on the provided data set. We’d like to inform the users and our site\
+    visitors that the provided dataset is quite imbalanced on several demographical facets, hence in cases when the probability,\
+    we’ve made sure to output a qualifying statement: “we’ll need more information to guess your next move!”'')
 
     # dictionary with list object in values
     details = {
@@ -36,8 +42,6 @@ def app():
     # creating a Dataframe object 
     df = pd.DataFrame(details)
     st.write(df)
- 
- 
 
     st.write("Data Source [link](https://www.kaggle.com/arashnic/hr-analytics-job-change-of-data-scientists)")
 
